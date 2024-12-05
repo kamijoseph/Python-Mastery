2be654996592ae05be48f49ede4d7d1782963a70
Kami Joseph <josephkiarie561@gmail.com>
2024-12-05 15:02:04 +0300

    Decors

--- program 82.py
+++ program 82.py
@@ -1,2 +1,15 @@
 
 #Python decorators
+# A function that extends the behaviour of another function without modifying the base function.
+# Passes the base function as an argument to the decorator.
+
+def add_sprinkles(func):
+    def wrapper():
+        return func()
+    return wrapper()
+
+@add_sprinkles        
+def get_ice_cream():
+    print("Here is your Ice Cream budd")
+    
+get_ice_cream()
