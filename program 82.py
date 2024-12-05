911d00e3835e0501fcabf59897fc43da8568f7e5
Kami Joseph <josephkiarie561@gmail.com>
2024-12-05 15:31:27 +0300

    Fudge

--- program 82.py
+++ program 82.py
@@ -4,13 +4,20 @@
 # Passes the base function as an argument to the decorator.
 
 def add_sprinkles(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         print("*Add your sprinkles friend.*")
-        func()
+        func(*args, **kwargs)
     return wrapper
 
+def add_fudge(func):
+    def wrapper(*args, **kwargs):
+        print("*Add your desired fudge friend*")
+        func(*args, **kwargs)
+    return wrapper
+
+@add_fudge
 @add_sprinkles        
-def get_ice_cream():
-    print("Here is your Ice Cream budd")
+def get_ice_cream(flavor):
+    print(f"Here is your {flavor} Ice Cream budd")
     
-get_ice_cream()
+get_ice_cream("Chocolate")
