6d7b816d5aa7b979c3e1f7958eee93e1ed1e46d7
Kami Joseph <josephkiarie561@gmail.com>
2024-12-21 22:34:38 +0300

    Bank 3rd func

--- Banking.py
+++ Banking.py
@@ -64,4 +64,27 @@ class SavingsAcct(InterestRewardsAcct):
             self.getBalance()
         except BalanceException as error:
             print(f"Withdraw interrupted: {error}!")
-        
+            
+if __name__ == "__main__":
+    dave = BankAccount(1000, "Dave")
+    sara = BankAccount(1000, "Sara")
+    jim = InterestRewardsAcct(1000, "Jim")
+    blaze = SavingsAcct(1000, "Blaze")
+
+    dave.getBalance()
+    dave.deposit(100000)
+    dave.withdraw(10)
+    dave.transfer(10000, sara)
+
+
+    sara.deposit(500)
+    sara.getBalance()
+
+
+    jim.getBalance()
+    jim.deposit(1000)
+    jim.transfer(100, dave)
+
+    blaze.getBalance()
+    blaze.deposit(1000)
+    blaze.transfer(1000, sara)
