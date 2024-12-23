c4f7296230de73f4284c16f894c6235e8c40dcb1
Kami Joseph <josephkiarie561@gmail.com>
2024-12-23 19:19:36 +0300

    Feed Pet

--- Virtual_Pet.py
+++ Virtual_Pet.py
@@ -3,5 +3,16 @@
 
 # VirtualPetClass
 class VirtualPet:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 50
+        self.happiness = 50
+        self.energy = 50
+        
+    def feed(self):
+        if self.hunger > 0:
+            self.hunger -= 10
+            self.happiness += 5
+            print(f"You fed {self.name}. Hunger decreased, happiness increased")
+        else:
+            print(f"self.name is already full.")
