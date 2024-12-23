1e243890ffd3283c67b5a8fc9cc3eb6b79d0e9d2
Kami Joseph <josephkiarie561@gmail.com>
2024-12-23 11:23:03 +0300

    Book created

--- library_sys.py
+++ library_sys.py
@@ -1,3 +1,13 @@
 
 # Library management sys
 print("This is The Fate's Algorithm Library")
+
+class Book:
+    def __init__(self, title, author, genre, isbn):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.isbn = isbn
+        
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.genre}, ISBN{self.isbn})"
