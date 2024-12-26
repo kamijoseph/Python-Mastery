c3dde07d73429f1fe444be8a5363576db01690ec
Kami Joseph <josephkiarie561@gmail.com>
2024-12-26 15:57:02 +0300

    Fetch Alerts

--- Weather_tracker.py
+++ Weather_tracker.py
@@ -20,3 +20,31 @@ class WeatherTracker:
             return weather_info
         else:
             return None
+    
+    def fetch_forecast(self, location):
+        url = f"{self.base_url}forecast?q={location}&appid={self.api_key}&units=metric"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json
+            forecasts = []
+            for entry in data['list'][:5]:  # Fetching the next 5 time slots
+                forecast = {
+                    'datetime': entry['dt_txt'],
+                    'temperature': entry['main']['temp'],
+                    'description': entry['weather'][0]['description']
+                }
+                forecasts.append(forecast)
+            return forecasts
+        else:
+            return None
+        
+    def fetch_alerts(self, location):
+        # Assuming we have access
+        url = f"{self.base_url}alerts?q={location}&appid={self.api_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            alerts = data.get('alerts', [])
+            return alerts if alerts else "No weather alerts."
+        else:
+            return None
