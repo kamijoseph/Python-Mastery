aaf2363f9f8be6451e0d7b68b14d62c597807a0b
Kami Joseph <josephkiarie561@gmail.com>
2025-01-01 14:11:03 +0300

    Load Tasks

--- todoapp.py
+++ todoapp.py
@@ -1,3 +1,14 @@
 
 # Python To do List App with JSON
 import json
+
+# Storage file
+FILE_NAME = "tasks.json"
+
+def load_tasks():
+    try:
+        with open(FILE_NAME, "r") as file:
+            return json.load
+    except FileNotFoundError:
+        return []
+    
