b9fabd94f7c60c6b5bd0afc1f4c01fa954bd91fa
Kami Joseph <josephkiarie561@gmail.com>
2025-01-02 15:47:57 +0300

    Check Winners

--- tictactoe.py
+++ tictactoe.py
@@ -5,3 +5,22 @@ def print_board(board):
         print(" | ".join(row))
         print("_" * 5)
         
+def check_winner(board):
+    # Check rows and columns
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != " ":
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != " ":
+            return board[0][i]
+        
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return board[0][2]
+
+    # Check for a tie
+    if all(cell != " " for row in board for cell in row):
+        return "Tie"
+    
+    return None
