6d4e5b686edcfc4cb95dbbe7c74c4a82c0549d67
Kami Joseph <josephkiarie561@gmail.com>
2025-01-02 18:47:08 +0300

    Main Module

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,17 @@
 
 # A password generator
-def generator():
+def generate_password(length):
     pass
 
 def main():
-    pass
+    print("Welcome to th PassWord Generator!")
+    try:
+        length = int(input("Enter the desired length of the password. Above 4 characters: "))
+        password = generate_password(length)
+        print(f"Your generated password is \n{password}")
+    except ValueError as e:
+        print(f"Error {e}")
+    
 
 if __name__ == "__main__":
     main()
