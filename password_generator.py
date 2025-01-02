d87b55b62a321f8c71371bd20cbd31a1a92f902c
Kami Joseph <josephkiarie561@gmail.com>
2025-01-02 19:14:02 +0300

    Finalizing....

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,32 @@
 
 # A password generator
+import random
+import string
+
 def generate_password(length):
-    pass
+    if length < 4:
+        raise ValueError("Password length must be at least 4 characters for a strong password.")
+    
+    #Character Sets for Password
+    uppercase = string.ascii_uppercase
+    lowercase = string.ascii_lowercase
+    digits = string.digits
+    special_char = string.punctuation
+    
+    password = [
+        random.choice(uppercase),
+        random.choice(lowercase),
+        random.choice(digits),
+        random.choice(special_char)
+    ]
+    
+    # Fill the rest of the password length with random choices from all sets
+    all_chars = uppercase + lowercase + digits + special_char
+    password += random.choices(all_chars, k=length-4)
+    
+    # Shuffle the password to randomize character order
+    random.shuffle(password)
+    return ''.join(password)
 
 def main():
     print("Welcome to th PassWord Generator!")
